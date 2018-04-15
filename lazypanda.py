cba431eda02b5b381dc0777590af704ebb43c27e
Royal Bhati <royal240596@gmail.com>
2018-04-15 12:28:08 +0530

    added new function

--- lazypanda.py
+++ lazypanda.py
@@ -6,9 +6,13 @@ import pandas as pd
 def usage():
 	print(''' Currently supported functions
 
-	checknull: checks if theres any null values in particular column of your dataset
+	checknull_column: checks if theres any null values in particular column of your dataset
 				  >>    usage: 
 				  		checknull('column_name') Note:Make sure your data is loaded as df
+	
+	checknull: checks if theres any null values in entire dataset
+				  >>    usage: 
+				  		checknull() Note:Make sure your data is loaded as df
 	
 	sigmoid: returns sigmoid of any number or array specified
 				  >>    usage:
@@ -17,8 +21,11 @@ def usage():
 				  			>>0.5''')	  	
 
 
-def checknull(f):
-    return df[f].isna().value_counts()
+def checknull_column(f):
+    return df[f].isna().values.any(),('sum :',df[f].isna().values.sum())
+
+ddef checknull():
+    return df.isna().values.any(),('sum :',df.isna().values.sum())		
 
 def sigmoid(x):
 	return 1/(1+np.exp(-x))
